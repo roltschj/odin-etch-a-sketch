c740167476c8ed247fee0e2c1804cdfe15bc2a37
roltschj <68574801+roltschj@users.noreply.github.com>
2024-06-14 22:41:57 -0400

    add mouseover functionality, change grid construction to arbitrary # of squares (default 16x16)

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
 document.addEventListener('DOMContentLoaded', function(event) {
-    function addRow() {
+    function addRow(width, height) {
+
+        const squareHeight = (800 / height) + "px";
+        const squareWidth = (800 / width) + "px";
         const newDiv = document.createElement("div");
 
         newDiv.className = "row";
 
-        for ( j = 0; j < 4; j++ ) {
+        for ( j = 0; j < width; j++ ) {
             const newSquare = document.createElement("div");
             newSquare.className = "gridSquare";
+            newSquare.style.height = squareHeight;
+            newSquare.style.width = squareWidth;
+            newSquare.addEventListener("mouseover", (event) => {
+                newSquare.style.backgroundColor = "black";
+            })
             newDiv.appendChild(newSquare);
         }
 
         const container = document.querySelector('#container');
 
+        newDiv.style.height = squareHeight;
         container.appendChild(newDiv);
     }
 
-    for ( i = 0; i < 4; i++ ) {
-        addRow();
+    function makeGrid(width = 16, height = 16) {
+        for ( i = 0; i < height; i++ ) {
+            addRow(width, height);
+        }
     }
+
+    makeGrid();
+
 });
